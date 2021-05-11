48b9006d482000bc0d9dd05667af289c9fbc10a0
iWumbo19 <rancourtvideo@gmail.com>
2021-05-11 15:45:50 -0500

    Add project files.

---
+++ Character_Creator/Character.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Creator
+{
+    class Character
+    {
+
+        //Basic character information
+        private string chaName;
+        private string chaRace;
+        private string chaClass;
+        private int chaLevel;
+        private string chaAlightment;
+
+        //Ability Scores/Modifiers
+        private int strScore;
+        private int dexScore;
+        private int conScore;
+        private int wisScore;
+        private int intScore;
+        private int chaScore;
+        private int strMod;
+        private int dexMod;
+        private int conMod;
+        private int wisMod;
+        private int intMod;
+        private int chaMod;
+
+        //Skill Modifiers and Proficiencies
+        private int acroScore;
+        private bool acroProf;
+        private int animalScore;
+        private bool animalProf;
+        private int arcanaScore;
+        private bool arcanaProf;
+        private int athleticsScore;
+        private bool athleticsProf;
+        private int deceptionScore;
+        private bool deceptionProf;
+        private int historyScore;
+        private bool historyProf;
+        private int insightScore;
+        private bool insightProf;
+        private int intimidationScore;
+        private bool intimidationProf;
+        private int investigationScore;
+        private bool investigationProf;
+        private int medicineScore;
+        private bool medicineProf;
+        private int natureScore;
+        private bool natureProf;
+        private int perceptionScore;
+        private bool perceptionProf;
+        private int performaceScore;
+        private bool performanceProf;
+        private int persuasionScore;
+        private bool persuasionProf;
+        private int religonScore;
+        private bool religonProf;
+        private int slieghtScore;
+        private bool slieghtProf;
+        private int stealthScore;
+        private bool stealthProf;
+        private int survivalScore;
+        private bool survivalProf;
+
+        //Misc Statisics
+        private int chaSpeed;
+        private int chaAC;
+        private int hitDie;
+        private int currentHealth;
+        private int maxHealth;
+        private int passiveWis;
+        private int passiveInt;
+        private int initiativeBonus;
+
+        private void InitializeStats()
+        {
+            
+        }
+    }
+}
---
+++ Character_Creator/Dictionaries.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Creator
+{
+    static class Dictionaries
+    {
+        static public Dictionary<int, int> levelProf = new Dictionary<int, int>()
+        {
+            {1,2 },
+            {2,2 },
+            {3,2 },
+            {4,2 },
+            {5,3 },
+            {6,3 },
+            {7,3 },
+            {8,3 },
+            {9,4 },
+            {10,4 },
+            {11,4 },
+            {12,4 },
+            {13,5 },
+            {14,5 },
+            {15,5 },
+            {16,5 },
+            {17,6 },
+            {18,6 },
+            {19,6 },
+            {20,6 },
+        };
+        static public Dictionary<int, int> scoreMod = new Dictionary<int, int>()
+        {
+            {1,-5 },
+            {2,-4 },
+            {3,-4 },
+            {4,-3 },
+            {5,-3 },
+            {6,-2 },
+            {7,-2 },
+            {8,-1 },
+            {9,-1 },
+            {10,0 },
+            {11,0 },
+            {12,1 },
+            {13,1 },
+            {14,2 },
+            {15,2 },
+            {16,3 },
+            {17,3 },
+            {18,4 },
+            {19,4 },
+            {20,5 },
+            {21,5 },
+            {22,6 },
+            {23,6 },
+            {24,7 },
+            {25,7 },
+            {25,8 },
+            {26,8 },
+            {27,9 },
+            {28,9 },
+            {29,10 },
+            {30,10 },
+        };
+        static public Dictionary<string, int> classDie = new Dictionary<string, int>()
+        {
+            {"sorcerer",6 },
+            {"wizard",6 },
+            {"artificer",8 },
+            {"bard",8 },
+            {"cleric",8 },
+            {"druid",8 },
+            {"monk",8 },
+            {"rouge",8 },
+            {"warlock",8 },
+            {"fighter",10 },
+            {"paladin",10 },
+            {"ranger",10 },
+            {"barbarian",12 },
+        };
+    }
+}
