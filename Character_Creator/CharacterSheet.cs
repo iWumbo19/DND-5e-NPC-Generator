de9b0b8068944e32403310d26d456a3fa0a1f588
iWumbo19 <rancourtvideo@gmail.com>
2021-06-02 12:10:47 -0400

    Created Forms. Need to do homework

---
+++ Character_Creator/CharacterSheet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Character_Creator
+{
+    public partial class CharacterSheet : Form
+    {
+        public CharacterSheet()
+        {
+            InitializeComponent();
+        }
+
+        private void CharacterSheet_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
